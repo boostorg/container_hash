3521c417b59b657e9f845f41b4f844fd0d462e3a
Daniel James <daniel@calamity.org.uk>
2018-01-20 17:30:58 +0000

    Small program to write out hash config info

---
+++ test/hash_info.cpp
@@ -0,0 +1,15 @@
+
+// Copyright 2017 Daniel James.
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+
+// Not a test, just a small program to write out configuration info
+
+#include <boost/container_hash/hash.hpp>
+#include <iostream>
+
+int main() {
+    std::cout << "BOOST_HASH_HAS_STRING_VIEW: "
+        << BOOST_HASH_HAS_STRING_VIEW
+        << std::endl;
+}
