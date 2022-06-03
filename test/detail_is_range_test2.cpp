e0e86a14139382102f6665d19a4fc9dc849dfc08
Peter Dimov <pdimov@gmail.com>
2022-06-03 20:01:26 +0300

    Add detail_is_range_test2.cpp

---
+++ test/detail_is_range_test2.cpp
@@ -0,0 +1,34 @@
+// Copyright 2022 Peter Dimov
+// Distributed under the Boost Software License, Version 1.0.
+// https://www.boost.org/LICENSE_1_0.txt
+
+#if defined(_MSC_VER)
+# pragma warning(disable: 4714) // forceinline not inlined
+#endif
+
+#if defined(__clang__)
+# pragma clang diagnostic ignored "-Wsign-conversion"
+#endif
+
+#include <boost/container_hash/detail/is_range.hpp>
+#include <boost/core/lightweight_test_trait.hpp>
+#include <boost/config.hpp>
+#include <boost/filesystem/path.hpp>
+#if !defined(BOOST_NO_CXX17_HDR_FILESYSTEM)
+# include <filesystem>
+#endif
+
+int main()
+{
+    using boost::hash_detail::is_range;
+
+    BOOST_TEST_TRAIT_FALSE((is_range< boost::filesystem::path >));
+    BOOST_TEST_TRAIT_FALSE((is_range< boost::filesystem::path const >));
+
+#if !defined(BOOST_NO_CXX17_HDR_FILESYSTEM)
+    BOOST_TEST_TRAIT_FALSE((is_range< std::filesystem::path >));
+    BOOST_TEST_TRAIT_FALSE((is_range< std::filesystem::path const >));
+#endif
+
+    return boost::report_errors();
+}
