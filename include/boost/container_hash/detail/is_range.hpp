b8d315fac5c76e58f1c9cceda4e188dc630f11ac
Peter Dimov <pdimov@gmail.com>
2022-06-03 20:14:07 +0300

    Update is_range, disable -Wvariadic-macros for GCC

--- include/boost/container_hash/detail/is_range.hpp
+++ include/boost/container_hash/detail/is_range.hpp
@@ -10,7 +10,6 @@
 #include <boost/type_traits/declval.hpp>
 #include <boost/type_traits/is_same.hpp>
 #include <boost/type_traits/remove_cv.hpp>
-#include <boost/type_traits/enable_if.hpp>
 #include <boost/config.hpp>
 #include <boost/config/workaround.hpp>
 #include <iterator>
@@ -22,9 +21,9 @@ namespace hash_detail
 
 #if !defined(BOOST_NO_CXX11_DECLTYPE) && !defined(BOOST_NO_SFINAE_EXPR) && !BOOST_WORKAROUND(BOOST_GCC, < 40700)
 
-template<class T, class It> true_type is_range_check( It first, It last,
-    typename std::iterator_traits<It>::difference_type* = 0,
-    typename enable_if_< !is_same<typename remove_cv<T>::type, typename std::iterator_traits<It>::value_type>::value >::type* = 0 );
+template<class T, class It>
+    integral_constant< bool, !is_same<typename remove_cv<T>::type, typename std::iterator_traits<It>::value_type>::value >
+        is_range_check( It first, It last );
 
 template<class T> decltype( is_range_check<T>( declval<T const&>().begin(), declval<T const&>().end() ) ) is_range_( int );
 template<class T> false_type is_range_( ... );
