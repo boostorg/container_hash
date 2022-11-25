558d0ead17e62a95797b6334ef943352df71e448
Peter Dimov <pdimov@gmail.com>
2022-11-25 21:13:57 +0200

    Suppress std::complex<int> warning under clang-cl

--- test/hash_complex_test.cpp
+++ test/hash_complex_test.cpp
@@ -3,6 +3,8 @@
 // Distributed under the Boost Software License, Version 1.0. (See accompanying
 // file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 
+#define _SILENCE_NONFLOATING_COMPLEX_DEPRECATION_WARNING
+
 #include "./config.hpp"
 
 #if !defined(BOOST_HASH_TEST_EXTENSIONS)
