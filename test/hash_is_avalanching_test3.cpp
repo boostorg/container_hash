aceb3aed65af7eaf4abdeb2ab7313d79d0a7a357
joaquintides <joaquin@tid.es>
2025-05-27 10:38:36 +0200

    stopped using external std::hash for testing

--- test/hash_is_avalanching_test3.cpp
+++ test/hash_is_avalanching_test3.cpp
@@ -4,8 +4,6 @@
 
 #include <boost/container_hash/hash_is_avalanching.hpp>
 #include <boost/core/lightweight_test_trait.hpp>
-#include <functional>
-#include <string>
 #include <type_traits>
 
 struct X
@@ -23,14 +21,18 @@ struct Z
     using is_avalanching = std::false_type;
 };
 
+struct W
+{
+};
+
 int main()
 {
     using boost::container_hash::hash_is_avalanching;
 
-    BOOST_TEST_TRAIT_FALSE(( hash_is_avalanching< std::hash<std::string > > ));
     BOOST_TEST_TRAIT_TRUE(( hash_is_avalanching< X > ));
     BOOST_TEST_TRAIT_TRUE(( hash_is_avalanching< Y > ));
     BOOST_TEST_TRAIT_FALSE(( hash_is_avalanching< Z > ));
+    BOOST_TEST_TRAIT_FALSE(( hash_is_avalanching< W > ));
 
     return boost::report_errors();
 }
